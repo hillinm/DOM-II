d023977c2ffbf626c11154d5add08b2429cff170
Mark Hillin <mark.hillin@gmail.com>
2020-09-08 11:24:27 -0500

    rockets working

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -6,16 +6,28 @@ const greenRocket = document.querySelector('.blocks .block--green')
 const pinkRocket = document.querySelector('.blocks .block--pink')
 const grayRocket = document.querySelector('.blocks .block--gray')
 
+redRocket.addEventListener('click', () => {
+    gsap.to(blueRocket, {duration: 2, y: 0})
+    gsap.to(redRocket, {duration: 2, y: 0})
+    gsap.to(greenRocket, {duration: 2, y: 0})
+    gsap.to(pinkRocket, {duration: 2, y: 0})
+    gsap.to(grayRocket, {duration: 2, y: 0})
+});
 
 blueRocket.addEventListener('click', () => {
     gsap.to(blueRocket, {duration: 2, y: -120})
     gsap.to(redRocket, {duration: 2, y: 120})
+    gsap.to(greenRocket, {duration: 2, y: 0})
+    gsap.to(pinkRocket, {duration: 2, y: 0})
+    gsap.to(grayRocket, {duration: 2, y: 0})
 });
 
 greenRocket.addEventListener('click', () => {
     gsap.to(greenRocket, {duration: 2, y: -240})
     gsap.to(blueRocket, {duration: 2, y: 120})
     gsap.to(redRocket, {duration: 2, y: 120})
+    gsap.to(pinkRocket, {duration: 2, y: 0})
+    gsap.to(grayRocket, {duration: 2, y: 0})
 })
 
 pinkRocket.addEventListener('click', () => {
@@ -23,6 +35,7 @@ pinkRocket.addEventListener('click', () => {
     gsap.to(greenRocket, {duration: 2, y: 120})
     gsap.to(blueRocket, {duration: 2, y: 120})
     gsap.to(redRocket, {duration: 2, y: 120})
+    gsap.to(grayRocket, {duration: 2, y: 0})
 })
 
 grayRocket.addEventListener('click', () => {
